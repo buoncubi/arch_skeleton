7309fb36d8d6ade022ce0e363359d4eb87c8d9e1
buoncubi <buon_luca@yahoo.com>
2022-11-02 15:09:08 +0100

    Review documentation.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
+# This file runs with `catkin_make`, do not run it manually!
 setup_args = generate_distutils_setup(
     packages=['arch_skeleton'],
     package_dir={'': 'utilities'}
